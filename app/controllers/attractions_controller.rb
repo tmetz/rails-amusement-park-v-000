b437a24fbbbdf48220aa969744ca69ec2e4b3d07
Tammy Metz <tammy@tammymetz.com>
2019-04-28 20:50:06 +0000

    Automatically backed up by Learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -11,3 +11,15 @@ class AttractionsController < ApplicationController
   def new
     @attraction = Attraction.new
   end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def user_params
+    params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating, :tickets, :min_height)
+  end
+end
